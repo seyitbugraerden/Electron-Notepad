fba1997369a08117c58093cc723cdb1a900c52e1
seyitbugraerden <seyitbugraerden@gmail.com>
2024-11-12 12:56:05 +0300

    locale string value

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,3 +1,12 @@
+import { contextBridge } from 'electron'
+
 if (!process.contextIsolated) {
   throw new Error('contextIsolation must be enabled in the BrowserWindow')
+}
+try {
+  contextBridge.exposeInMainWorld('context', {
+    locale: navigator.language
+  })
+} catch (error) {
+  console.log(error)
 }
